fd2a4b49794139264eab4a14636deb2701b8ce19
bielferr <gabrielferreiradesouza5513@gmail.com>
2025-03-01 01:13:26 -0300

    upload do hero

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,26 +1,51 @@
-import { Grid } from "@mui/material";
-import { styled } from "@mui/system";
+import { Button, Grid, Typography } from "@mui/material";
+import { Container, styled } from "@mui/system";
 import Avatar from "../../../../assets/images/avatar.jpg";
-
+import DownloadIcon from "@mui/icons-material/Download";
+import ContactsIcon from "@mui/icons-material/Contacts";
 const Hero = () => {
   const StyledHero = styled("div")(() => ({
     backgroundColor: "black",
-    padding: "20px",
-    textAlign: "center",
+    height: "100vh",
   }));
 
   const StyledImg = styled("img")(() => ({
-    width: "30%",
+    width: "100%",
     borderRadius: "50%",
   }));
 
   return (
     <StyledHero>
-      <Grid container spacing={2}>
-        <Grid item xs={4}></Grid>
-        <Grid item xs={8}></Grid>
-      </Grid>
-      <StyledImg src={Avatar} alt="Avatar" />
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <StyledImg src={Avatar} alt="Avatar" />
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Typography color="primary" variant="h1" textAlign={"center"}>
+              Gabriel Ferreira
+            </Typography>
+            <Typography color="primary" variant="h2" textAlign={"center"}>
+              I'am Desenvolvedor Web
+            </Typography>
+
+            <Grid container display={"flex"} justifyContent={"center"}>
+              <Grid item xs={12} md={4} display={"flex"} justifyContent={"center"}>
+                <Button>
+                  <DownloadIcon />
+                  Download CV
+                </Button>
+              </Grid>
+              <Grid item xs={12} md={4} display={"flex"} justifyContent={"center"}>
+                <Button>
+                  <ContactsIcon />
+                  Contact me
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
     </StyledHero>
   );
 };
